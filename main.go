4e01cf2d35294de50873664bb65b7afd06479973
brownpanthera <alifaizan1983@gmail.com>
2026-01-19 00:30:26 +0530

    refactor GetWindowDetails and add title_name and class_name

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	win "github.com/tilinwindow/win32"
+	"fmt"
+
+	"github.com/tilinwindow/win32"
 )
 
 func main() {
-	win.WindowDetails()
+	handles := win32.GetWindowDetails()
+	for _, i := range handles {
+		title, _ := win32.GetWindowTextW(i)
+		class, _ := win32.GetClassName(i)
+
+		fmt.Printf("Title: %s | Class: %s\n", title, class)
+	}
 }
--- win32/window.go
+++ win32/window.go
@@ -1,29 +1,53 @@
-package win
+package win32
 
 import (
-	"fmt"
+	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
-func WindowDetails() {
-	xs := windows.GetForegroundWindow()
-	fmt.Println(xs)
+var (
+	user32             = windows.NewLazySystemDLL("user32.dll")
+	procGetWindowTextW = user32.NewProc("GetWindowTextW")
+	procGetClassNameW  = user32.NewProc("GetClassNameW")
+)
 
-	var result []uintptr
-
-	cb := windows.NewCallback(EnumWindowsProc)
-	windows.EnumWindows(cb, unsafe.Pointer(&result))
-
-	for _, v := range result {
-		fmt.Println(v)
+func GetWindowDetails() []uintptr {
+	allOpenHandles := []uintptr{}
+	EnumWindowsProc := func(hwnd uintptr, lparam uintptr) uintptr {
+		allOpenHandles = append(allOpenHandles, hwnd)
+		return 1
 	}
+	callbackFun := windows.NewCallback(EnumWindowsProc)
+
+	windows.EnumWindows(callbackFun, nil)
+
+	return allOpenHandles
 }
 
-func EnumWindowsProc(hwnd uintptr, lparam uintptr) uintptr {
-	resPtr := (*[]uintptr)(unsafe.Pointer(lparam))
-	*resPtr = append(*resPtr, hwnd)
+/*
+load the dll
+get the addr of the function in the dll
+call the function at that addr
+*/
 
-	return 1
+func GetWindowTextW(hwnd uintptr) (string, error) {
+	buf := make([]uint16, 256)
+	maxNumberOfChar := 256
+	r1, _, err := procGetWindowTextW.Call(hwnd, uintptr(unsafe.Pointer(&buf[0])), uintptr(maxNumberOfChar))
+	if r1 == 0 {
+		if err != nil && err != windows.ERROR_SUCCESS {
+			return "", err
+		}
+		return "", nil
+	}
+	title := syscall.UTF16ToString(buf[:r1])
+	return title, nil
+}
+
+func GetClassName(hwnd uintptr) (string, error) {
+	buf := make([]uint16, 256)
+	procGetClassNameW.Call(hwnd, uintptr(unsafe.Pointer(&buf[0])), 256)
+	return syscall.UTF16ToString(buf), nil
 }
